e1287a0e7b83033936aaedc46fda3b525de848ca
Natalie Mclaren <>
2017-11-11 20:10:31 +0000

    Read Serial input to get initial depth of river

--- Flood-Sensor.ino
+++ Flood-Sensor.ino
@@ -1,19 +1,65 @@
-int analogPin = 0;     // Connect Pin 3 of the maxbotix to A0. Pin 4 of the maxbotix must be disconnected!
-int rawVal = 0;           // variable to store the value read
+
+// Connect Pin 3 of the maxbotix to A0.
+int analogPin = 0; 
+// Variable to store the value read   
+int rawVal = 0;   
+// Variable to store the distance
 int distance = 0;
+
+// Initial depth
+float depth = 0;    
 
 void setup()
 
 {
   // Outputs information to your serial port
-  Serial.begin(9600);          //  setup serial
+  //  Setup serial
+  Serial.begin(9600);  
+
+   // delay(3000);
+
+  // Wait for serial to connect
+  while (!Serial){}
+    
+  int currentDistanceToRiverTopMM;
+
+  // Check for incoming serial data:
+  //if (Serial.available() > 0) {
+    Serial.println("Set up");
+
+    char initialRiverDepthMMInput[10];
+    String inStr;
+
+    // Whilst inStr is null, do nothing, skip
+    while ((inStr = Serial.readString()) == NULL){}
+
+    const int initialRiverDepthMM = inStr.toInt();
+    Serial.println(initialRiverDepthMM); 
+  //}
 }
 
 void loop()
 
 {
-  rawVal = analogRead(analogPin);    // Read a raw value
-  distance = rawVal * 5;             // As per datasheet (to get mm)
-  Serial.println(distance);             // Output value
+  // Read a raw value
+  rawVal = analogRead(analogPin);
+  // As per datasheet (to get mm)   
+  distance = rawVal * 5;  
+  // Output value  
+  Serial.println(distance);   
+
+  // Check for incoming serial data:
+  if (Serial.available() > 0) {
+  
+    String str;
+
+    // Whilst str is null, do nothing, skip
+    while ((str = Serial.readString()) == NULL){}
+    
+    // Convert string to int
+    Serial.println(str.toInt());
+
+  }
+  
   delay(2000);
 }
