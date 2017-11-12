c97ed0573720c91ca86e1173e14e981d073259d5
Natalie Mclaren <>
2017-11-12 19:31:45 +0000

    Add ticker library & add functionality to only send (print) data if above/below a certain threshold

--- Flood-Sensor.ino
+++ Flood-Sensor.ino
@@ -1,65 +1,133 @@
+#include "Ticker.h"
+
 
 // Connect Pin 3 of the maxbotix to A0.
 int analogPin = 0; 
-// Variable to store the value read   
-int rawVal = 0;   
-// Variable to store the distance
-int distance = 0;
 
 // Initial depth
-float depth = 0;    
+int initialRiverDepth;
+int rangeDifferenceThreshold = 50;
+int distanceToRiverBed;
+int lastMeasurementSent;
+int currentRiverLevel;
+
+volatile bool canStart = false;
+
+
+void startReadingProcess();
+
+Ticker tick;
+
 
 void setup()
 
 {
+ 
   // Outputs information to your serial port
   //  Setup serial
-  Serial.begin(9600);  
+  Serial.begin(9600); 
 
-   // delay(3000);
-
+  tick.setCallback(startReadingProcess);
+  tick.setInterval(2000);
+  
+  // Start the ticker.
+  tick.start(); 
+  
   // Wait for serial to connect
   while (!Serial){}
     
-  int currentDistanceToRiverTopMM;
+  int currentDistanceToRiverTop;
 
   // Check for incoming serial data:
   //if (Serial.available() > 0) {
     Serial.println("Set up");
 
-    char initialRiverDepthMMInput[10];
+    char initialRiverDepthInput[10];
     String inStr;
+    int initialDistanceToRiverTop;
 
     // Whilst inStr is null, do nothing, skip
     while ((inStr = Serial.readString()) == NULL){}
-
-    const int initialRiverDepthMM = inStr.toInt();
-    Serial.println(initialRiverDepthMM); 
+    initialRiverDepth = inStr.toInt();
+    //Serial.println(initialRiverDepth); 
+    initialDistanceToRiverTop = analogRead(analogPin) * 5;
+    distanceToRiverBed = initialRiverDepth + initialDistanceToRiverTop;
   //}
+}
+
+int getCurrentMeasurement()
+{
+  // Read a raw value
+  int rawVal = analogRead(analogPin);
+  // As per datasheet (to get mm)   
+  int currentDistanceToRiverTop = rawVal * 5;  
+  // Output value  
+
+  return distanceToRiverBed - currentDistanceToRiverTop;
+}
+
+bool isCurrentWorthSending(int currentMeasurement)
+{
+  return (abs(currentMeasurement - lastMeasurementSent)) >= rangeDifferenceThreshold;
+}
+
+
+bool sendMeasurement(int currentMeasurement)
+{
+  Serial.println("Measurement Sent:");
+  Serial.println(currentMeasurement);
+  return true;
+}
+
+void printCurrentMeasurement()
+{
+  Serial.println("Current Measurement: ");
+  Serial.println(getCurrentMeasurement());
+}
+
+void attemptToSendMeasurement(int currentMeasurement)
+{
+  if(isCurrentWorthSending(currentMeasurement))
+  {
+    if(sendMeasurement(currentMeasurement))
+      lastMeasurementSent = currentMeasurement;
+  }
+  //else ignore current measurement
+  
+}
+
+void startReadingProcess() {
+
+  currentRiverLevel = getCurrentMeasurement();
+  printCurrentMeasurement();
+  attemptToSendMeasurement(currentRiverLevel);
+}
+
+// Engineering menu
+void loadEngineeringMenu() {
+
+  Serial.println("Loading engineering menu...");
+  
+  int menuOption = Serial.readString().toInt();
+  
+  if(menuOption == 1) {
+    printCurrentMeasurement();
+  } else if (menuOption == 2) {
+    Serial.println("Do something else");
+  }
 }
 
 void loop()
 
 {
-  // Read a raw value
-  rawVal = analogRead(analogPin);
-  // As per datasheet (to get mm)   
-  distance = rawVal * 5;  
-  // Output value  
-  Serial.println(distance);   
 
-  // Check for incoming serial data:
-  if (Serial.available() > 0) {
+  tick.update();
   
-    String str;
-
-    // Whilst str is null, do nothing, skip
-    while ((str = Serial.readString()) == NULL){}
-    
-    // Convert string to int
-    Serial.println(str.toInt());
-
-  }
+  // attachInterrupt(Serial, loadEngineeringMenu, HIGH);
   
-  delay(2000);
+//  if(Serial) {
+//    // Engineering menu set up
+//    loadEngineeringMenu();
+//  } 
+
 }
