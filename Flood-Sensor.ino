9868c9fb43cfe1c9e124db63cb99241d689d778f
Natalie Mclaren <>
2017-11-19 20:12:12 +0000

    Add menu function to check SD card memory space

--- Flood-Sensor.ino
+++ Flood-Sensor.ino
@@ -2,6 +2,7 @@
 #include <SPI.h>
 #include <SD.h>
 
+
 const byte ledPin = 1;
 const byte interruptPin = 13;
 volatile byte state = LOW;
@@ -24,7 +25,9 @@ void startReadingProcess();
 
 // Ticker tick;
 File myFile;
-
+SdVolume volume;
+SdFile root;
+Sd2Card card;
 
 void setup()
 
@@ -84,6 +87,60 @@ bool isCurrentWorthSending(int currentMeasurement)
   return (abs(currentMeasurement - lastMeasurementSent)) >= rangeDifferenceThreshold;
 }
 
+
+void checkCardMemory()
+{ 
+  Serial.print("\nPrinting card info...");
+
+  if (!card.init(SPI_HALF_SPEED, 4)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("* is a card inserted?");
+    Serial.println("* is your wiring correct?");
+    Serial.println("* did you change the chipSelect pin to match your shield or module?");
+    return;
+  } else {
+    Serial.println("Wiring is correct and a card is present.");
+  }
+  
+  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
+  if (!volume.init(card)) {
+    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+    return;
+  }
+
+  Serial.print("Clusters:          ");
+  Serial.println(volume.clusterCount());
+  Serial.print("Blocks x Cluster:  ");
+  Serial.println(volume.blocksPerCluster());
+
+  Serial.print("Total Blocks:      ");
+  Serial.println(volume.blocksPerCluster() * volume.clusterCount());
+  Serial.println();
+
+  // print the type and size of the first FAT-type volume
+  uint32_t volumesize;
+  Serial.print("Volume type is:    FAT");
+  Serial.println(volume.fatType(), DEC);
+
+  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
+  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
+  volumesize /= 2;                           // SD card blocks are always 512 bytes (2 blocks are 1KB)
+  Serial.print("Volume size (Kb):  ");
+  Serial.println(volumesize);
+  Serial.print("Volume size (Mb):  ");
+  volumesize /= 1024;
+  Serial.println(volumesize);
+  Serial.print("Volume size (Gb):  ");
+  Serial.println((float)volumesize / 1024.0);
+
+  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
+  root.openRoot(volume);
+
+  // list all files in the card with date and size
+  root.ls(LS_R | LS_DATE | LS_SIZE);
+}
+
+
 void printToLog(int lastMeasurementSent)
 {
 
@@ -91,6 +148,7 @@ void printToLog(int lastMeasurementSent)
   if (!SD.begin(4)) {
     Serial.println("initialization failed!");
   }
+
 
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
@@ -120,7 +178,6 @@ void printToLog(int lastMeasurementSent)
     Serial.println("error opening logger.txt");
   }
 }
-
 
 
 bool sendMeasurement(int currentMeasurement)
@@ -166,10 +223,16 @@ void loadEngineeringMenu() {
     
    if(menuOption == "1\n") {
      printCurrentMeasurement();
-   } 
+   }
+
+   if(menuOption == "5\n") {
+      Serial.println("Testing read/write...");
+      printToLog(5);
+   }
    
-   if (menuOption == "2\n") {
-    Serial.println("Do something else");
+   if (menuOption == "6\n") {
+      // Serial.println("Do something else...");
+      checkCardMemory();
    }
   };
   
@@ -181,8 +244,6 @@ void setBringUpMenu()
 {
   bringUpMenu = true;
 }
-
-
 
 void loop()
 
