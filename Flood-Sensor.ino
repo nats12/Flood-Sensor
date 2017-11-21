d2aa934df3db6777ea47048633a7607a8dbaac58
Natalie Mclaren <>
2017-11-21 12:55:10 +0000

    Engineering menu option 7 - change measurement period

--- Flood-Sensor.ino
+++ Flood-Sensor.ino
@@ -19,7 +19,7 @@ int currentRiverLevel;
 
 volatile bool canStart = false;
 volatile bool bringUpMenu = false;
-
+int period = 5000;
 
 void startReadingProcess();
 
@@ -205,6 +205,14 @@ void attemptToSendMeasurement(int currentMeasurement)
   
 }
 
+
+void changeMeasurementPeriod() 
+{
+  
+}
+
+
+
 void startReadingProcess() {
 
   currentRiverLevel = getCurrentMeasurement();
@@ -212,32 +220,59 @@ void startReadingProcess() {
   attemptToSendMeasurement(currentRiverLevel);
 }
 
+void changeMeasurementPeriod(String minutes)
+{
+  // Update the delay period
+  period = minutes.toInt();
+}
+
+
+
 // Engineering menu
-void loadEngineeringMenu() {
+void loadEngineeringMenu() 
+{
 
   Serial.println("Loading engineering menu...");
+  Serial.println("Please select an option:");
+  Serial.println("1: Print current measurement");
+  Serial.println("5: Test read/write functionality on SD Card");
+  Serial.println("6: Print details of SD Card");
+  Serial.println("7: Change measurement period");
   
   String menuOption;
+  String minutes;
   
   while((menuOption = Serial.readString()) != "99\n"){
     
    if(menuOption == "1\n") {
+     // Print last sent measurement
      printCurrentMeasurement();
    }
 
    if(menuOption == "5\n") {
+      // Test read/write on SD card
       Serial.println("Testing read/write...");
       printToLog(5);
    }
-   
+
    if (menuOption == "6\n") {
-      // Serial.println("Do something else...");
+      // Print details of SD card space
       checkCardMemory();
    }
+
+   if (menuOption == "7\n") {
+      // Insert a new measurement period
+      Serial.println("Insert a new measurement period");
+      
+      while((minutes = Serial.readString()) == NULL){};
+
+      // Call function to update global variable period to minutes
+      changeMeasurementPeriod(minutes);
+   }
+   
   };
   
   bringUpMenu = false;
-
 }
 
 void setBringUpMenu() 
@@ -256,7 +291,9 @@ void loop()
   if(bringUpMenu) {
     loadEngineeringMenu();
   }
-  
-  delay(2000);
+
+  Serial.println("Current measurement period is..");
+  Serial.println(period);
+  delay(period);
 
 }
